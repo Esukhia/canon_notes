2660498b9ee81ab65c4d3ed51b41bbe81f2546c5
drupchen <hhdrupchen@gmail.com>
2016-10-18 11:00:58 +0200

    reorganise the repo

---
+++ 1-a-reinsert_notes/csv_contains_pardrang_col.py
@@ -0,0 +1,12 @@
+from PyTib.common import open_file
+import os
+
+bad = []
+for f in os.listdir('./input'):
+    if f.endswith('csv'):
+        content = open_file('./input/'+f)
+        if content.startswith('མཚན་བྱང་།,པར་གྲངས།,ཤོག་གྲངས།,1,མཚན་གྲངས།,པར་མ།,མཆན།') or content.startswith('མཚན་བྱང་།,པར་གྲངས།,ཤོག་གྲངས།,1,མཆན་གྲངས།,པར་མ།,མཆན།'):
+            bad.append(f)
+for b in sorted(bad):
+    print(b)
+print(len(bad))

b536cb57de7b5cf68c2f3a8b378b3f64a2817965
drupchen <hhdrupchen@gmail.com>
2016-11-29 23:29:32 +0100

    Shetring finished !

--- 4-a-final_formatting/reinsert_page_nums.py
+++ 4-a-final_formatting/reinsert_page_nums.py
@@ -4,9 +4,19 @@ from collections import deque
 
 pattern = {
     '1-དབུ་མ།_དབུ་མ་རྩ་བའི་ཚིག་ལེའུར་བྱས་པ་ཤེས་རབ།': [
-        (1, 5, 5),
+        ('start', 1, 'ན'),
+        ('end', 19, 'ན'),
+        [1, 5, 5],
         7,
-        6]
+        6
+    ],
+    '1-སྤྲིང་ཡིག།_བཤེས་པའི་སྤྲིང་ཡིག': [
+        ('start', 40, 'བ'),
+        ('end', 46, 'བ'),
+        [4],
+        7,
+        3
+    ],
 }
 
 
@@ -29,7 +39,7 @@ def page_num(counter, side):
     num = str(counter)
     for n in equivalences:
         num = num.replace(n[0], n[1])
-    return '[{}{}]'.format(num, side)
+    return '\[{}{}\]'.format(num, side)
 
 
 def create_page(lines, size, counter, side):
@@ -43,14 +53,15 @@ in_path = './output/2-3-a_reinserted'
 out_path = './output/3-1-page_reinserted'
 for f in os.listdir(in_path):
     work_name = f.replace('_a_reinserted.txt', '')
+    print('processing', work_name)
     content = open_file('{}/{}'.format(in_path, f))
     text, notes = content.split('\n\n')
     lines = deque(text.replace('\n', ' ').split('a'))
 
     pages = []
-    text_pattern = pattern[work_name]
-    counter = 1
-    side = 'ན'
+    text_pattern = pattern[work_name][2:]
+    counter = pattern[work_name][0][1]
+    side = pattern[work_name][0][2]
 
     # beginning pages
     for num in text_pattern[0]:

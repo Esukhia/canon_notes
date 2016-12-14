61ae39c1e59cccdc975c2008f3730f60bae71b45
drupchen <hhdrupchen@gmail.com>
2016-12-14 22:44:43 +0100

    finish formatting the _segmented.txt file to include the note tag and the note with the right note_numbers

--- 4-a-final_formatting/1-2_segment_final.py
+++ 4-a-final_formatting/1-2_segment_final.py
@@ -6,13 +6,13 @@ import os
 
 
 # copied from 0-1_apply_note_choices.py
-def format_footnote(bad, good):
+def format_footnote(bad, good, note_num):
     def agree_zhas(chosen_ed):
         last_syl = pre_process(chosen_ed, mode='syls')[-1]
         return Agreement().part_agreement(last_syl, 'ཞེས')
 
     zhas = agree_zhas(good)
-    return '[^seg?]: མ་དཔེར་{}། བྱུང་ཡང་། {}། {}་བཅོས།'.format(bad, good, zhas)
+    return '[^seg{}]: མ་དཔེར་{}། བྱུང་ཡང་། {}། {}་བཅོས།'.format(note_num, bad, good, zhas)
 
 
 def parse_data(string):
@@ -31,7 +31,7 @@ for f in os.listdir(in_path):
     work_name = f.replace('_unmarked.txt', '')
     content = open_file('{}/{}'.format(in_path, f))
     segmented = Segment().segment(content, danying=True)
-    new_notes = ['\n']
+    new_notes = defaultdict(tuple)
     suggestions = defaultdict(list)
     for r in replacements:
         bad_segmented = Segment().segment(r[0])
@@ -41,17 +41,24 @@ for f in os.listdir(in_path):
         for b in bads:
             if b in segmented:
                 segmented = segmented.replace(' {} '.format(b), ' @{} '.format(b))
-                suggestions[' @{} '.format(b)] = []
-                new_notes.append(format_footnote(r[0], r[1]))
-    misspellings = [str(num)+line for num, line in enumerate(segmented.split('\n')) if '#' in line]
+                key = ' @{} '.format(b.strip('་'))
+                comment = '\t{} >> {}'.format(b.strip('་'), r[1])
+                if comment not in suggestions[key]:
+                    suggestions[key].append(comment)
+                new_notes[key] = (r[0], r[1])
+    misspellings = ['{} {}'.format(num+1, line) for num, line in enumerate(segmented.split('\n')) if '#' in line]
 
+    sugg_num = 1
     for num, line in enumerate(segmented.split('\n')):
         for s in suggestions:
-            if s in line:
-                suggestions[s].append(str(num) + line)
-    all_suggestions = [a for k in tib_sort(suggestions.keys()) for a in suggestions[k]+['\n'] if suggestions[k] != []]
+            if s in line or s.strip()+'་ ' in line:
+                note_tag = '{}[^seg{}]'.format(new_notes[s][1], sugg_num)
+                suggestion = '{} {}\n\t{}\t{}'.format(num+1, line, note_tag, format_footnote(new_notes[s][0], new_notes[s][1], sugg_num))
+                suggestions[s].append(suggestion)
+                sugg_num += 1
+    all_suggestions = ['\n'.join(suggestions[k])+'\n' for k in tib_sort(suggestions.keys()) if len(suggestions[k]) > 1]
     mistakes = misspellings + ['\n'] + all_suggestions
-    write_file("{}/{}_segmented.txt".format(out_path, work_name), '\n'.join(mistakes+new_notes))
+    write_file("{}/{}_segmented.txt".format(out_path, work_name), '\n'.join(mistakes))
 
 # copy the corrected file in post_seg if it does not exist
 for f in os.listdir('output/0-3-corrected'):

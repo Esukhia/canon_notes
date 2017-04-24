d8f444d6758a444893a7593bfdf40ec25a2584e4
drupchen <hhdrupchen@gmail.com>
2017-04-25 00:52:57 +0200

    data update

--- layout/layout.sh
+++ layout/layout.sh
@@ -3,11 +3,11 @@
 # sudo apt-get install docx2txt xlsx2csv
 # xlsx2csv requires to escape the spaces in the file names. The easiest is to remove all spaces from file names.
 
-FILES=../4-a-final_formatting/output/3-3-final/*.txt
+FILES="../4-a-final_formatting/output/3-3-final/*.txt"
 for f in $FILES
 do
   filename="${f#../4-a-final_formatting/output/3-3-final/}"
   filename="${filename%_final.*}"
-  echo "Converting $f"
-  `pandoc -f markdown -t docx $f -o ./output/$filename.docx`
+  echo "Converting ${f/' '/'\ '}"
+  `pandoc -f markdown -t docx "${f/' '/'\ '}" -o ./output/$filename.docx`
 done

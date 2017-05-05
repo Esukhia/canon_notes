ebd4e52073a1364ecadbf86a56038c204fa7b631
drupchen <hhdrupchen@gmail.com>
2017-05-05 11:26:06 +0200

    script update do not check if the file already exists. This was useful when we were hand-modifying the files, but since we only modify the input files of reinsertion.py, it only creates problems to copy in case the file does not exist.

--- 4-a-final_formatting/1-3_copy_post_seg.py
+++ 4-a-final_formatting/1-3_copy_post_seg.py
@@ -6,8 +6,8 @@ def copy_missing(origin_path, target_path, origin_name_end, target_name_end):
     for f in os.listdir(origin_path):
         work_name = f.replace(origin_name_end, '')
         existing = [g.replace(target_name_end, '') for g in os.listdir(target_path)]
-        if work_name not in existing:
-            write_file('{}/{}{}'.format(target_path, work_name, target_name_end), open_file('{}/{}{}'.format(origin_path, work_name, origin_name_end)))
+        #if work_name not in existing:
+        write_file('{}/{}{}'.format(target_path, work_name, target_name_end), open_file('{}/{}{}'.format(origin_path, work_name, origin_name_end)))
 
 
 origin_path = 'output/1-3-post_seg'
